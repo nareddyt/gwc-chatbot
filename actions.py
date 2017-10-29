479bcd0b53f0c8621464f57277ec762582dc4fba
Tejasvi Nareddy <tejunareddy@gmail.com>
2017-10-29 11:38:08 -0400

    Added a get time action

--- actions.py
+++ actions.py
@@ -1,18 +1,36 @@
+import datetime
+
+
 def choose_action(action, data):
     text = "Looking for action..."
 
     if action == "hello":
-        text = hello_action(data)
-    elif action == "something":
-        # TODO
-        pass
+        text = hello(data)
+    elif action == "time":
+        text = get_time(data)
     else:
         text = "No action matched!"
 
     return text
 
 
-def hello_action(data):
-    # Simple action that responds with hello world
+def hello(data):
+    """
+    Says "Hello World" to the user
+    """
+
     text = "Hello world"
+    return text
+
+
+def get_time(data):
+    """
+    Tells the user the current time
+    """
+
+    now = datetime.datetime.now()
+    hour = now.hour
+    minute = now.minute
+
+    text = "The time is %d:%d" % hour, minute
     return text
